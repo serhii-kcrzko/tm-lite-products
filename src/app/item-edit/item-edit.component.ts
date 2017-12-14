e2131443b509103cb0786cbf3d55b99aff44cba9
Serhii Kocherezhko <Serhii Kocherezhko>
2017-12-14 21:40:44 +0200

    Retrofit product edit history page

--- src/app/item-edit/item-edit.component.ts
+++ src/app/item-edit/item-edit.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-
 import { BackendService } from '../backend.service';
+import { sortBy } from 'lodash/sortBy';
+import { Router } from '@angular/router';
+
+import * as moment from 'moment';
+import 'moment/locale/uk';
+moment.locale('uk');
 
 @Component({
   selector: 'app-item-edit',
@@ -10,23 +15,41 @@ import { BackendService } from '../backend.service';
   styleUrls: ['./item-edit.component.css']
 })
 export class ItemEditComponent implements OnInit {
-  data: any = {};
+  data: any = [];
 
-  constructor(private http: Http, private db: BackendService) { }
+  constructor(private http: Http, private db: BackendService, private router: Router) { }
 
   getData() {
     return this.db.getProducts();
   }
 
   getProducts() {
-    this.getData().subscribe(data => {
-      this.data = data;
-    });
+    this.getData().subscribe(res => this.data = res);
   }
 
   ngOnInit() {
     this.getProducts();
     this.getData();
+  }
+
+  parseDate(date: string): string {
+    const parsedDate = moment(date);
+    return parsedDate.format('D MMMM YYYY, hh:mm:ss');
+  }
+
+  capitalize(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  getLocation(): string {
+    return this.router.url;
+  }
+
+  parseUser(email: string): string {
+    const login = email.split('@')[0];
+    const userName = login.split('.');
+    const [name, surname] = userName;
+    return `${this.capitalize(name)} ${this.capitalize(surname)}`;
   }
 
 }
